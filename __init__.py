55eba1bbaae3115319d9c9104c5093dbc81669cf
euan-gwd <euan@euangreenwood.com>
2026-01-12 22:06:39 +0000

    update

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@
 from .save_text_node import SimpleSaveText
 
 NODE_CLASS_MAPPINGS = {"SimpleSaveText": SimpleSaveText}
-NODE_DISPLAY_NAME_MAPPINGS = {"SimpleSaveText": "💾 Save & Copy Text"}
+NODE_DISPLAY_NAME_MAPPINGS = {"SimpleSaveText": "💾 View & Save Text"}
 WEB_DIRECTORY = "./js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]

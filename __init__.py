771c1d4f0f7c299691271f60924815c81d3cd172
euan-gwd <euan@euangreenwood.com>
2026-01-13 15:16:39 +0000

    add show text

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 # In __init__.py
 from .save_text_node import SimpleSaveText
+from .show_text import ShowText
 
-NODE_CLASS_MAPPINGS = {"SimpleSaveText": SimpleSaveText}
-NODE_DISPLAY_NAME_MAPPINGS = {"SimpleSaveText": "💾 View & Save Text"}
+NODE_CLASS_MAPPINGS = {"SimpleSaveText": SimpleSaveText, "ShowText|pysssss": ShowText,}
+NODE_DISPLAY_NAME_MAPPINGS = {"SimpleSaveText": "💾 View & Save Text", "ShowText|pysssss": "Show Text 🐍",}
 WEB_DIRECTORY = "./js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]

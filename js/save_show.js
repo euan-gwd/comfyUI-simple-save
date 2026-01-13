a1ab391cbf7b024ca977a753d2eb06ffecdb49da
euan-gwd <euan@euangreenwood.com>
2026-01-13 15:25:10 +0000

    fix save show

--- js/save_show.js
+++ js/save_show.js
@@ -6,33 +6,54 @@ app.registerExtension({
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
         if (nodeData.name === "SimpleSaveText") {
 
-            const onNodeCreated = nodeType.prototype.onNodeCreated;
-            nodeType.prototype.onNodeCreated = function () {
-                const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
+            function populate(text) {
+                if (this.widgets) {
+                    // Remove all widgets except the converted input widget if present
+                    const isConvertedWidget = +!!this.inputs?.[0]?.widget;
+                    for (let i = isConvertedWidget; i < this.widgets.length; i++) {
+                        this.widgets[i].onRemove?.();
+                    }
+                    this.widgets.length = isConvertedWidget;
+                }
 
-                // 1. Find or Create the display widget
-                const w = ComfyWidgets["STRING"](this, "display_text", ["STRING", { multiline: true }], app).widget;
+                const v = [...text];
+                if (!v[0]) v.shift();
+                for (let list of v) {
+                    if (!(list instanceof Array)) list = [list];
+                    for (const l of list) {
+                        const w = ComfyWidgets["STRING"](this, "display_text_" + (this.widgets?.length ?? 0), ["STRING", { multiline: true }], app).widget;
+                        w.inputEl.readOnly = true;
+                        w.inputEl.style.cursor = "text";
+                        w.inputEl.style.userSelect = "text";
+                        w.inputEl.style.color = "white";
+                        w.inputEl.placeholder = "Run character builder to see text...";
+                        w.value = l;
+                    }
+                }
 
-                // 2. Setup Selection/Copy properties
-                w.inputEl.readOnly = true;
-                w.inputEl.style.cursor = "text";
-                w.inputEl.style.userSelect = "text";
-                w.inputEl.style.color = "white";
-                w.inputEl.placeholder = "Run character builder to see text...";
-
-                this.size = [400, 250];
-                return r;
-            };
+                requestAnimationFrame(() => {
+                    const sz = this.computeSize();
+                    if (sz[0] < this.size[0]) sz[0] = this.size[0];
+                    if (sz[1] < this.size[1]) sz[1] = this.size[1];
+                    this.onResize?.(sz);
+                    app.graph.setDirtyCanvas(true, false);
+                });
+            }
 
             const onExecuted = nodeType.prototype.onExecuted;
             nodeType.prototype.onExecuted = function (message) {
                 onExecuted?.apply(this, arguments);
                 if (message?.text) {
-                    const w = this.widgets.find((w) => w.name === "display_text");
-                    if (w) {
-                        w.value = message.text[0];
-                    }
+                    populate.call(this, message.text);
                 }
+            };
+
+            // Optionally, set a default size on node creation
+            const onNodeCreated = nodeType.prototype.onNodeCreated;
+            nodeType.prototype.onNodeCreated = function () {
+                const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
+                this.size = [400, 250];
+                return r;
             };
         }
     },
